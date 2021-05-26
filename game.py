be2c24964fcefa0cc104a3f469b08ebebefa701d
acw573 <acw573@stern.nyu.edu>
2021-05-25 20:43:07 -0400

    Validate user inputs

--- game.py
+++ game.py
@@ -7,3 +7,20 @@ user_choice = input("Please choose one of 'rock','paper','scissors': ")
 
 #print(user_choice)
 print("USER CHOICE: ",user_choice)
+
+
+# validate the input such that only if it is one of the expected values will we continue
+# ...with the rest of the program otherwise we'll stop the program before it tries to do anything else
+#...and we'll ask the user to run the program again
+
+#double equal sign is for equality checking
+
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
+    print("VALID. KEEP GOING")
+else:
+        print("OOPS, INVALID INPUT. PLEASE TRY AGAIN.")
+
+exit()
+
+
+print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN.")
